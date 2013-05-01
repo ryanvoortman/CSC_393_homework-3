df4b91dbfdfe0f20fdfad3069fb90dc087535462
Ryan Voortman <ryanvoortman@loop-227-112.loop-wireless.depaul.edu>
2013-05-01 16:57:00 -0500

    Initial Commit

---
+++ CSC_393_homework#3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CSC_393_homework#3
+//
+//  Created by Ryan Voortman on 5/1/13.
+//  Copyright (c) 2013 Ryan Voortman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
